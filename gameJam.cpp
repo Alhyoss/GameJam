f2749eac990d3ecff6fd6e857590d4a3c0f91776
Alhyoss <simlardinois109@gmail.com>
2017-04-01 12:05:34 +0200

    commit

--- gameJam.cpp
+++ gameJam.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
+void handleEvents(sf::RenderWindow *window);
+
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    sf::RenderWindow *window = new sf::RenderWindow(sf::VideoMode(700, 700), "GameJam",
+                            sf::Style::Titlebar | sf::Style::Close);
+
+    while(window->isOpen())
+    {
+        handleEvents(window);
+
+        window->clear(sf::Color::Black);
+        window->display();
+    }
     return 0;
+}
+
+void handleEvents(sf::RenderWindow *window)
+{
+
+    sf::Event event;
+    while(window->pollEvent(event))
+    {
+        if(event.type == sf::Event::Closed)
+            window->close();
+    }
 }
